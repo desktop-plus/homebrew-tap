37e481cb9831a7a9499924247a06bc3b523076e4
EstebanForge <1696145+EstebanForge@users.noreply.github.com>
2026-09-10 11:18:48 -0300

    Use postflight_steps instead of deprecated postflight

--- Casks/desktop-plus.rb
+++ Casks/desktop-plus.rb
@@ -21,9 +21,8 @@ cask "desktop-plus" do
   binary "#{appdir}/Desktop Plus.app/Contents/Resources/app/static/desktop-plus-cli.sh",
          target: "desktop-plus-cli"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Desktop Plus.app"]
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{appdir}}/Desktop Plus.app"]
   end
 
   zap trash: [
